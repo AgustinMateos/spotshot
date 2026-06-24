886be60618ee94b001c00c932a7f616dcac52721
agustin mateos <agustinrotondomateos@gmail.com>
2026-06-24 13:19:54 -0300

    coruna

--- src/lib/constants/surfData.js
+++ src/lib/constants/surfData.js
@@ -9,6 +9,9 @@
 // ============================================================
 
 export const escuelas = [
+   { name: "Escuela La Wave Surf Coruña", region: "Galicia, España", label: "Escuela La Wave Surf Galicia" },
+   { name: "Escuela La Wave Surf Canarias", region: "Canarias, España", label: "Escuela La Wave Surf Canarias" },
+   { name: "Escuela La Wave Surf Somo", region: "Cantabria, España", label: "Escuela La Wave Surf Somo" },
   { name: "Escuela Cantabra de Surf", region: "Cantabria, España", label: "Escuela Cantabra de Surf - Cantabria, España" },
   { name: "Somo's Beach Surf School", region: "Cantabria, España", label: "Somo's Beach Surf School - Cantabria, España" },
   { name: "Sunset", region: "Cantabria, España", label: "Sunset - Cantabria, España" },
